88ab271af7e0736316d47f9ae5fd52f47a7795c7
beigirad <f.beigirad@gmail.com>
2018-06-23 10:42:42 +0430

    remove unused files

--- src/runner/Main.kt
+++ src/runner/Main.kt
@@ -4,9 +4,7 @@ import javax.swing.JFrame
 import javax.swing.SwingUtilities
 
 object Main {
-
-    @Throws(Exception::class)
-    @JvmStatic
+    
     fun main(args: Array<String>) {
         SwingUtilities.invokeLater {
             val frame = JFrame("Compiler Workshop")
