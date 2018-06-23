4534e413acdd2e97dedf217924c1ed37dc5c902b
beigirad <f.beigirad@gmail.com>
2018-06-23 10:31:51 +0430

    Initial commit

---
+++ src/runner/Main.kt
@@ -0,0 +1,19 @@
+package runner
+
+import javax.swing.JFrame
+import javax.swing.SwingUtilities
+
+object Main {
+
+    @Throws(Exception::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        SwingUtilities.invokeLater {
+            val frame = JFrame("Compiler Workshop")
+            frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+            frame.pack()
+            frame.isResizable = false
+            frame.isVisible = true
+        }
+    }
+}
